524e131c2bb6d4f8921fa889cbf74344669ac412
Ramo170 <icaro.editor170@gmail.com>
2026-02-04 11:35:45 -0300

    Subindo pasta frontend

---
+++ blog-frontend/app/Cadastro/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Cadastro() {
+  return (
+    <h1>Cadastro</h1>
+  );
+}
---
+++ blog-frontend/app/Login/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <h1>Login</h1>
+  );
+}
