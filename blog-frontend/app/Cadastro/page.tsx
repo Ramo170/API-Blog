800bf4a095c13754334edd281057ec3ca681b310
Ramo170 <icaro.editor170@gmail.com>
2026-02-05 10:27:13 -0300

    feat: cria tela de cadastro do usuario no frontend

--- blog-frontend/app/Cadastro/page.tsx
+++ blog-frontend/app/Cadastro/page.tsx
@@ -1,7 +1,54 @@
-import Image from "next/image";
+"use client";
+
+import { useRouter} from "next/navigation";
+import { useState } from "react";
 
 export default function Cadastro() {
+
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const router = useRouter()
+
+  const cadastrar = async (e:any) => {
+    e.preventDefault()
+
+    const res = await fetch("http://localhost:3001/usuarios", {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({email, senha})
+    })
+
+    if(res.ok){
+      alert("Usuário cadastrado com sucesso!")
+      router.push("/Login")
+    }else{
+
+    }
+
+  }
+
   return (
-    <h1>Cadastro</h1>
+    <main>
+      <h1>Cadastro</h1>
+      <form onSubmit={cadastrar}>
+        <input 
+        type="email" 
+        placeholder="Digite o seu email..."
+        value={email}
+        onChange={(e)=>{setEmail(e.target.value)}}
+        />
+
+        <input 
+        type="password" 
+        placeholder="Digite o sua senha..."
+        value={senha}
+        onChange={(e)=>{setSenha(e.target.value)}}
+        />
+
+        <button className="bg-blue-500 text-white p-2 rounde hover:bg-blue-600 transition-colors">
+           Cadastrar
+        </button>
+      </form>
+    </main>
   );
 }
